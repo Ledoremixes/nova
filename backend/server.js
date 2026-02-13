5f92fc21f0077409088520196f50efdde0cf678f
manuel <manuelledoremixes@gmail.com>
2026-02-13 09:48:58 +0100

    cors fixed

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,6 @@ app.options(/.*/, cors());
 
 const PORT = process.env.PORT || 4000;
 
-app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
